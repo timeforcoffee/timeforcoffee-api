4c9e3d03f728fd91f94079545bf8c4e6cb40d545
Christian Stocker <me@chregu.tv>
2023-02-01 22:33:39 +0100

    updates

--- build-docker.sh
+++ build-docker.sh
@@ -1,3 +1,3 @@
-docker build --target build -t docker.gitlab.liip.ch/chregu/repo/tfc-api:build .
+docker build  --platform x86_64 --target build -t docker.gitlab.liip.ch/chregu/repo/tfc-api:build .
 
-docker build -t docker.gitlab.liip.ch/chregu/repo/tfc-api:latest .
+docker build --platform x86_64 -t docker.gitlab.liip.ch/chregu/repo/tfc-api:latest .
