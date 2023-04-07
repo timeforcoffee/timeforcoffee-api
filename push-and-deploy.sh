3aa9cf3133c34ee4b8e65038a453d40f8a29fcf4
Christian Stocker <me@chregu.tv>
2023-04-07 18:40:12 +0200

    check rollout status

--- push-and-deploy.sh
+++ push-and-deploy.sh
@@ -1,2 +1,3 @@
 docker push docker.gitlab.liip.ch/chregu/repo/tfc-api:latest
 kubectl patch deployment tfc-api -n tfc -p "{\"spec\": {\"template\": {\"metadata\": { \"labels\": {  \"redeploy\": \"$(date +%s)\"}}}}}"
+kubectl rollout status deployment/tfc-api -n tfc --timeout=180s
